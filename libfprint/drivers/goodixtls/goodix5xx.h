db578363a720d5061965d4c66fcdc13a00636704
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2022-11-11 00:24:28 +0000

    chore(goodixtls5xx): add more docs

--- libfprint/drivers/goodixtls/goodix5xx.h
+++ libfprint/drivers/goodixtls/goodix5xx.h
@@ -53,12 +53,12 @@ struct _FpiDeviceGoodixTls5xxClass
 {
   FpiDeviceGoodixTlsClass    parent;
 
-  GoodixTls5xxGetMcuFn       get_mcu_cfg;
-  GoodixTls5xxProcessFrameFn process_frame;
-  GoodixTls5xxResetStateFn   reset_state;
+  GoodixTls5xxGetMcuFn       get_mcu_cfg; ///< provide the mcu config before fdt commands
+  GoodixTls5xxProcessFrameFn process_frame; ///< process a frame after it is decoded (e.g. crop it)
+  GoodixTls5xxResetStateFn   reset_state; ///< callback to reset the state, may be NULL
 
-  guint16                    scan_width;
-  guint16                    scan_height;
+  guint16                    scan_width; ///< width of the raw scanner image
+  guint16                    scan_height; ///< height of the raw scanner image
 
   const char               * firmware_version; ///< only needed if goodixtls5xx_check_firmware_version() is used
 
@@ -70,18 +70,48 @@ struct _FpiDeviceGoodixTls5xxClass
   int            reset_number; ///< only needed if goodixtls5xx_check_reset() is used
 };
 
+/**
+ * @brief Check the reply to a reset command
+ *
+ * @param dev
+ * @param success
+ * @param number
+ * @param user_data
+ * @param error
+ */
 void goodixtls5xx_check_reset (FpDevice *dev,
                                gboolean  success,
                                guint16   number,
                                gpointer  user_data,
                                GError   *error);
 
+/**
+ * @brief Check the reply to a firmware version query matches configured firmware
+ * @note Requires firmware_version field to be set
+ *
+ * @param dev
+ * @param firmware
+ * @param user_data
+ * @param error
+ */
 void goodixtls5xx_check_firmware_version (FpDevice *dev,
                                           gchar    *firmware,
                                           gpointer  user_data,
                                           GError   *error);
 
 
+/**
+ * @brief Check the reply to a preset psk query matched configured psk
+ * @note Requires psk_flags, psk_len, and psk fields to be set
+ *
+ * @param dev
+ * @param success
+ * @param flags
+ * @param psk
+ * @param length
+ * @param user_data
+ * @param error
+ */
 void goodixtls5xx_check_preset_psk_read (FpDevice *dev,
                                          gboolean  success,
                                          guint32   flags,
@@ -90,44 +120,112 @@ void goodixtls5xx_check_preset_psk_read (FpDevice *dev,
                                          gpointer  user_data,
                                          GError   *error);
 
+/**
+ * @brief Check the reply to an idle command
+ *
+ * @param dev
+ * @param user_data
+ * @param err
+ */
 void goodixtls5xx_check_idle (FpDevice *dev,
                               gpointer  user_data,
                               GError   *err);
 
+/**
+ * @brief Check the reply to uploading an mcu config
+ *
+ * @param dev
+ * @param success
+ * @param user_data
+ * @param error
+ */
 void goodixtls5xx_check_config_upload (FpDevice *dev,
                                        gboolean  success,
                                        gpointer  user_data,
                                        GError   *error);
 
+/**
+ * @brief Check the reply to a powerdown_scan_freq command
+ *
+ * @param dev
+ * @param success
+ * @param user_data
+ * @param error
+ */
 void goodixtls5xx_check_powerdown_scan_freq (FpDevice *dev,
                                              gboolean  success,
                                              gpointer  user_data,
                                              GError   *error);
 
+/**
+ * @brief General check for GoodixNoneCallback replies
+ *
+ * @param dev
+ * @param user_data state machine to advance
+ * @param error
+ */
 void goodixtls5xx_check_none (FpDevice *dev,
                               gpointer  user_data,
                               GError   *error);
 
+/**
+ * @brief General callback for GoodixDefaultCallback replies
+ *
+ * @param dev
+ * @param data
+ * @param len
+ * @param ssm State machine to advance (userdata)
+ * @param err
+ */
 void goodixtls5xx_check_none_cmd (FpDevice *dev,
                                   guint8   *data,
                                   guint16   len,
                                   gpointer  ssm,
                                   GError   *err);
 
+/**
+ * @brief Start a scan
+ * @note This is called automatically for you unless you overwrote change_state in FpImageDeviceClass
+ *
+ * @param dev
+ */
 void goodixtls5xx_scan_start (FpiDeviceGoodixTls5xx * dev);
 
+/**
+ * @brief Decode a goodixtls frame
+ * @details Decodes the weird 4/6 byte packing: https://blog.th0m.as/misc/fingerprint-reversing/
+ * @note Doesn't decrypt it
+ *
+ * @param frame
+ * @param frame_size
+ * @param raw_frame
+ */
 void goodixtls5xx_decode_frame (GoodixTls5xxPix * frame,
                                 guint32           frame_size,
                                 const guint8     *raw_frame);
 
 
+/**
+ * @brief Initalise the TLS for the device
+ * @note You probably want to call this directly after device activation
+ *
+ * @param dev
+ */
 void goodixtls5xx_init_tls (FpDevice * dev);
 
+/**
+ * @brief Save an image to pgm
+ * @details Designed for debugging purposes, shouldn't be used in a non-debug code path
+ *
+ * @param img
+ * @param path
+ * @return gboolean
+ */
 gboolean goodixtls5xx_save_image_to_pgm (FpImage    *img,
                                          const char *path);
 
 /**
- * @brief Squashes the 12 bit pixels of a raw frame into the 4 bit pixels used
+ * @brief Squashes the 2 byte pixels of a raw frame into the 1 byte pixels used
  * by libfprint.
  * @details Borrowed from the elan driver. We reduce frames to
  * within the max and min.
